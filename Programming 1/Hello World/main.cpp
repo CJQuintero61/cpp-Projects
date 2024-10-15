3456150da8eacaedbc86f374a626595d67adbc77
Christian Quintero <133067862+CJQuintero61@users.noreply.github.com>
2024-10-15 15:09:34 -0500

    Add files via upload

---
+++ Programming 1/Hello World/main.cpp
@@ -0,0 +1,16 @@
+/* The Hello World program is usually the first
+ * program students learning to code will make.
+ * Looking back at this, a full year into coding and in Data Structures,
+ * I've really learned a lot and love to code. I'll continue to learn
+ * and continue to improve my skills.
+ * Christian Quintero 10/15/2024
+ */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!\n";
+    return 0;
+}
